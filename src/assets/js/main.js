4b165ba125147340e5684261a04fc67317cb02eb
Tran Trung Tinh <TranTrungTinh@TinhBookPro.local>
2019-02-14 14:52:11 +0700

    first push glass on project

---
+++ src/assets/js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function() { 
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 50,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items:1,
+                // nav:true
+            },
+            600:{
+                items:3,
+                // nav:false
+            },
+            1000:{
+                items:4,
+                // nav:true,
+                // loop:false
+            }
+        }
+    });
+    AOS.init();
+
+    $('.owl-prev').click(function() {
+        $('.owl-carousel').trigger('prev.owl.carousel');
+    });
+    $('.owl-next').click(function() {
+        $('.owl-carousel').trigger('next.owl.carousel');
+    });
+
+    $('.content__video-thumbnail').click(function() {
+        $('#exampleModalCenter').modal('toggle')
+    })
+
+    $('#whatIsGlassOn').click(function() {
+        console.log('aaa');
+    })
+});
